8501286fbecd1a04e6349c78d398647b275f86ea
Rubens Rech <rubensch@icloud.com>
2018-12-28 21:29:28 -0200

    Add initial frontend struct to test png upload

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-let model = require('./model');
+const PNG = require('pngjs').PNG;
 
+// let model = require('./model');
+var app = express();
+var port = 3000;
 
-// Train
-// let trainEpochs = 1;
-// model.trainModel(trainEpochs);
+// > Configuration
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended : 'true' }));        // parse application/x-www-form-urlencoded
 
-// Evaluate
-// model.evaluateModel();
+// > Routes
+app.post('/api/rec', function(req, res) {
+    var imgBase64 = req.body.base64;
+    imgBase64 = imgBase64.split(',')[1];
+    var imgData = Buffer.from(imgBase64, 'base64');    
+    console.log(imgData.length);
+});
 
-model.predict();
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+// otherwise ...
+app.all('*', function(req, res) {
+    res.sendFile(__dirname + '/public/notfound.html');
+});
+ 
+app.listen(port);
+console.log("App listening on port " + port);
